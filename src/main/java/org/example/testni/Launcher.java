ed20cc570037d071173d1447580e88b7b093680a
ErminStarcevic <ermin.starcevic.23@size.ba>
2025-12-11 15:58:02 +0100

    initial commit

---
+++ src/main/java/org/example/testni/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.testni;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
